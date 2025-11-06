aceb5e0a7ead5ac928e80d53245be035d4e1f3b5
Neezic <joaolucasdealmeida1@gmail.com>
2025-11-06 09:20:15 -0300

    organização

--- app/src/main/java/com/example/quizandroid/ui/theme/navigation/MainActivity.kt
+++ app/src/main/java/com/example/quizandroid/ui/theme/navigation/MainActivity.kt
@@ -1,17 +1,10 @@
-package com.example.quizandroid
+package com.example.quizandroid.ui.theme.navigation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.quizandroid.ui.theme.QuizAndroidTheme
+import com.example.quizandroid.ui.theme.auth.TelaLogin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/quizandroid/ui/theme/ranking/TelaHistorico.kt
+++ app/src/main/java/com/example/quizandroid/ui/theme/ranking/TelaHistorico.kt
@@ -1,4 +1,4 @@
-package com.example.quizandroid
+package com.example.quizandroid.ui.theme.ranking
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
