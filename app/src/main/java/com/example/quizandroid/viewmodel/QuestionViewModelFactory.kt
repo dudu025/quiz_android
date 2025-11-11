0559b685326594f6cd0a8883728baf4f6fa40d23
MATEUS RIBEIRO AGAPEJEV DE ANDRADE <mateus.andrade03@cs.up.edu.br>
2025-11-11 10:53:58 -0300

    Esqueleto do Quiz

---
+++ app/src/main/java/com/example/quizandroid/viewmodel/QuestionViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.quizandroid.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.quizandroid.data.repository.QuizRepository
+
+class QuestionViewModelFactory(
+    private val repository: QuizRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
